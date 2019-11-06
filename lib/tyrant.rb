db8d336694ffc6d313c1d003bd9c4b84ae453e05
Nick Sutterer <apotonick@gmail.com>
2019-11-06 11:57:10 +0100

    more files!

--- lib/tyrant.rb
+++ lib/tyrant.rb
@@ -1,6 +1,16 @@
 require "tyrant/version"
 
+require "trailblazer/activity"
+require "trailblazer/activity/dsl/linear"
+require "trailblazer/developer"
+
 module Tyrant
-  class Error < StandardError; end
-  # Your code goes here...
+  module_function
+
+  def call(activity, ctx)
+    signal, (ctx, _) = Trailblazer::Developer.wtf?(activity, [ctx, {}])
+    return ctx, signal.to_h[:semantic] == :success
+  end
 end
+
+require "tyrant/auth"
